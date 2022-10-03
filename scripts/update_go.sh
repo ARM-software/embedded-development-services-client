673d5fc3655dd3c5c4362ccd496b03318b1ec556
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2022-10-03 10:24:54 +0100

    Update update_go.sh

--- scripts/update_go.sh
+++ scripts/update_go.sh
@@ -2,20 +2,20 @@
 
 # Dependabot will only update the Dockerfiles however we need to share the version of Go in multiple places
 # So this script will fetch the version of Go from one of the Dockerfiles and then use this to update the:
-# CircleCI config
+# CI workflow definition
 # The README
 # The go.mod files
 # The changes will be committed using the commit_changes.sh script
 
 function update() {
-  DOCKERFILE="Dockerfile.acceptancetest"
-
+  DOCKERFILE="Dockerfile.client"
+  
   GO_FULL_VERSION=`grep -o 'golang:[0-9.]*' ${DOCKERFILE} | cut -d ":" -f2`
   GO_SHORT_VERSION=`echo ${GO_FULL_VERSION} | sed 's/\.[^.]*$//'`
 
-  sed -i -e "s/golang:[0-9.]*$/golang:${GO_FULL_VERSION}/g" .circleci/config.yml
+  sed -i -e "s/go_version: [\"0-9.]*$/go_version: \"${GO_FULL_VERSION}\"/g" .github/workflows/ci.yml
   sed -i -e "s/go-v[0-9.]*/go-v${GO_FULL_VERSION}/g" README.md
-  sed -i -e "s/^go [0-9.]*$/go ${GO_SHORT_VERSION}/g" acceptancetests/go.mod
+  sed -i -e "s/^go [0-9.]*$/go ${GO_SHORT_VERSION}/g" vhtk8s/go.mod
 
   git status
 }
