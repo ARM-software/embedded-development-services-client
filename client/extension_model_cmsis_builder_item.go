8688b13f684118273e0fa38cf7e37ec516547d92
acabarbaye <acabarbaye@users.noreply.github.com>
2023-01-18 17:24:37 +0000

    :sparkles: Automatic changes -> ✨ New server autogenerated files [ci skip]

--- client/extension_model_cmsis_builder_item.go
+++ client/extension_model_cmsis_builder_item.go
@@ -56,7 +56,7 @@ func (m *BuilderIterator) HasNext() bool {
 	return m.currentIndex < len(m.elements)
 }
 
-func (m *BuilderIterator) GetNext() (item *interface{}, err error) {
+func (m *BuilderIterator) GetNext() (item interface{}, err error) {
 	if m.currentIndex < 0 {
 		err = errors.New("incorrect element index")
 		return
@@ -65,7 +65,7 @@ func (m *BuilderIterator) GetNext() (item *interface{}, err error) {
 		err = errors.New("no more items")
 		return
 	}
-	element := interface{}(m.elements[m.currentIndex])
+	element := m.elements[m.currentIndex]
 	item = &element
 	m.currentIndex++
 	return
