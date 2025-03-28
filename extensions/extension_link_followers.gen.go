bc07a211b162b7db3dfd9a2ab9140fd6838fcf22
aorabdel <44305804+aorabdel@users.noreply.github.com>
2025-03-28 15:21:49 +0000

    :sparkles: Automatic changes -> ✨ New server autogenerated files [ci skip]

--- extensions/extension_link_followers.gen.go
+++ extensions/extension_link_followers.gen.go
@@ -2,13 +2,6 @@
  * Copyright (C) 2020-2025 Arm Limited or its affiliates and Contributors. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-
-//nolint:gocritic,stylecheck
-/*
- * Copyright (C) 2020-2025 Arm Limited or its affiliates and Contributors. All rights reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-
 package client
 
 import (
