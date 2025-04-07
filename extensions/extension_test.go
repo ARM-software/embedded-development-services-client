eb0856b50b966eb92614c3162b976f4e5019cb37
aorabdel <44305804+aorabdel@users.noreply.github.com>
2025-04-07 15:32:30 +0000

    :sparkles: Automatic changes -> ✨ New server autogenerated files [ci skip]

---
+++ extensions/extension_test.go
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2020-2025 Arm Limited or its affiliates and Contributors. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package client
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestClientExtension(t *testing.T) {
+	// Placeholder test for codeclimate
+	require.Nil(t, nil)
+}
