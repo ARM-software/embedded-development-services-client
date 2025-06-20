3c42594538e32010c3fd1bfb8384f7beaa727a35
Kem-Gov <14895121+Kem-Gov@users.noreply.github.com>
2025-06-20 14:31:45 +0000

    :sparkles: Automatic changes -> ✨ New server autogenerated files [ci skip]

--- client/model_fpga_admin_item.go
+++ client/model_fpga_admin_item.go
@@ -31,6 +31,8 @@ type FPGAAdminItem struct {
 	Metadata NullableCommonMetadata `json:"_metadata"`
 	// The unique identifier of the user
 	Name string `json:"name"`
+	// Optional human-readable name of the FPGA admin member.
+	Title NullableString `json:"title,omitempty"`
 }
 
 type _FPGAAdminItem FPGAAdminItem
@@ -131,6 +133,48 @@ func (o *FPGAAdminItem) SetName(v string) {
 	o.Name = v
 }
 
+// GetTitle returns the Title field value if set, zero value otherwise (both if not set or set to explicit null).
+func (o *FPGAAdminItem) GetTitle() string {
+	if o == nil || IsNil(o.Title.Get()) {
+		var ret string
+		return ret
+	}
+	return *o.Title.Get()
+}
+
+// GetTitleOk returns a tuple with the Title field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+// NOTE: If the value is an explicit nil, `nil, true` will be returned
+func (o *FPGAAdminItem) GetTitleOk() (*string, bool) {
+	if o == nil {
+		return nil, false
+	}
+	return o.Title.Get(), o.Title.IsSet()
+}
+
+// HasTitle returns a boolean if a field has been set.
+func (o *FPGAAdminItem) HasTitle() bool {
+	if o != nil && o.Title.IsSet() {
+		return true
+	}
+
+	return false
+}
+
+// SetTitle gets a reference to the given NullableString and assigns it to the Title field.
+func (o *FPGAAdminItem) SetTitle(v string) {
+	o.Title.Set(&v)
+}
+// SetTitleNil sets the value for Title to be an explicit nil
+func (o *FPGAAdminItem) SetTitleNil() {
+	o.Title.Set(nil)
+}
+
+// UnsetTitle ensures that no value is present for Title, not even an explicit nil
+func (o *FPGAAdminItem) UnsetTitle() {
+	o.Title.Unset()
+}
+
 func (o FPGAAdminItem) MarshalJSON() ([]byte, error) {
 	toSerialize,err := o.ToMap()
 	if err != nil {
@@ -144,6 +188,9 @@ func (o FPGAAdminItem) ToMap() (map[string]interface{}, error) {
 	toSerialize["_links"] = o.Links.Get()
 	toSerialize["_metadata"] = o.Metadata.Get()
 	toSerialize["name"] = o.Name
+	if o.Title.IsSet() {
+		toSerialize["title"] = o.Title.Get()
+	}
 	return toSerialize, nil
 }
 
